3efb30afece94454a64486b3c58b52e6ed79967a
Jason Norwood-Young <jason@10layer.com>
2019-04-25 12:24:11 +0200

    Now with login!

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 require("dotenv").config();
 const axios = require("axios");
+const qs = require("qs");
+
+axios.interceptors.request.use((request) => {
+	if (request.data && request.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+		request.data = qs.stringify(request.data);
+	}
+	return request;
+  });
 
 (async () => {
+	try {
+		const token_url = `https://api.bloomberg.com/syndication/token`;
+		const auth = Buffer.from(`${process.env.KEY}:${process.env.SECRET}`).toString('base64')
+		var token_result = (await axios({
+			url: token_url, 
+			method: "post",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Referer": "https://content-service.bloomberg.com/auth/login",
+				"Origin": "https://content-service.bloomberg.com",
+				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+				"Authorization": `Basic ${auth}`
+			},
+			withCredentials: true,
+			data: {
+				username: process.env.USERNAME,
+				password: process.env.PASSWORD,
+				remember: false,
+				grant_type: "password",
+			}
+		})).data;
+	} catch(err) {
+		console.error(err);
+		process.exit(1);
+	}
 	const url = `${process.env.API_ENDPOINT}?fields=provider_uid&filter[provider]=${process.env.PROVIDER}`;
 	console.log("Fetching articles", url);
 	var storedArticleData = (await axios.get( url, {
@@ -14,7 +47,7 @@ const axios = require("axios");
 	var storedArticles = storedArticleData.data;
 
 	// Set up Axios
-	axios.defaults.headers.common = {'Authorization': `Bearer ${process.env.TOKEN}`}
+	axios.defaults.headers.common = {'Authorization': `Bearer ${token_result.access_token}`}
 	// Load the site
 	console.log("Loading Articles", process.env.URL);
 	try {
