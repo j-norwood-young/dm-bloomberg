9b67ca16fcaec48a57e0fd915a8e8e6a1db3ca47
Jason Norwood-Young <jason@10layer.com>
2019-11-25 14:10:12 +0000

    Bloomberg working

--- index.js
+++ index.js
@@ -2,95 +2,153 @@ require("dotenv").config();
 const axios = require("axios");
 const qs = require("qs");
 
+const headers = {
+	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:71.0) Gecko/20100101 Firefox/71.0",
+	"Origin": "https://mercury.bloomberg.com",
+	"Content-Type": "application/json;charset=utf-8",
+	"Accept": "application/json, text/plain, */*",
+	"Accept-Language": "en-US,en;q=0.5",
+	"Referer": "https://mercury.bloomberg.com/login",
+	"DNT": 1,
+}
+
 axios.interceptors.request.use((request) => {
 	if (request.data && request.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
 		request.data = qs.stringify(request.data);
 	}
 	return request;
-  });
+});
 
 (async () => {
+	console.time("Bloomberg");
 	try {
-		const token_url = `https://api.bloomberg.com/syndication/token`;
-		const credentials = {
-			client: {
-				id: process.env.KEY,
-				secret: process.env.SECRET
-			},
-			auth: {
-				tokenHost:`https://api.bloomberg.com`,
-				tokenPath: `/syndication/token`
-			}
-		};
-		const oauth2 = require('simple-oauth2').create(credentials);
-		// Get the access token object.
-		const tokenConfig = {
-			username: process.env.USERNAME,
-			password: process.env.PASSWORD,
-		};
-		// Save the access token
+		// Log In
 		try {
-			const result = await oauth2.clientCredentials.getToken(tokenConfig);
-			var accessToken = oauth2.accessToken.create(result);
+			console.log(`Signing in at ${process.env.LOGIN_URL}`);
+			const result = await axios.post(`${process.env.LOGIN_URL}`, {
+				email: process.env.USERNAME,
+				password: process.env.PASSWORD
+			}, {
+				headers,
+			});
+			var accessToken = result.data.token;
 		} catch (error) {
-			console.log('Access Token Error', error.message);
+			throw(error.message);
 		}
-		
+
+		// Fetch list of existing articles
+		const url = `${process.env.API_ENDPOINT}/article?fields=provider_uid&filter[provider]=${process.env.PROVIDER}&limit=100&sort[date_created]=-1`;
+		console.log("Fetching articles", url);
+		var storedArticleData = (await axios.get(url, {
+			auth: {
+				username: process.env.API_USERNAME,
+				password: process.env.API_PASSWORD
+			}
+		})).data;
+		console.log("Fetched articles", storedArticleData.count);
+		var storedArticles = storedArticleData.data;
+
+		// Search articles from Bloomberg
+		try {
+			var articles = (await axios.post(process.env.SEARCH_URL, {
+				"query": "",
+				"filters": {
+					"global": { "publishedDate": new Date().toISOString() },
+					"media": {
+						"text": true,
+						"photo": false,
+						"video": false
+					},
+					"dateRange": {
+						"before": null,
+						"after": null
+					},
+					"photo": {
+						"landscape": false,
+						"portrait": false,
+						"creative": false,
+						"editorial": false
+					},
+					"video": {
+						"shortForm": true,
+						"longForm": true,
+						"english": true,
+						"spanish": false,
+						"japanese": false
+					},
+					"languages": [],
+					"feeds": [],
+					"contexts": {
+						"topics": [],
+						"companies": [],
+						"people": [],
+						"brands": [],
+						"series": []
+					}
+				},
+				"limit": 30
+			}, 
+			{
+				headers: Object.assign(headers, {
+					"Referer": "https://mercury.bloomberg.com/search",
+					"Cookie": `login=${accessToken}`
+				})
+			})).data.results;
+			// console.log({ articles });
+		} catch (error) {
+			// console.log(error);
+			throw (error.message);
+		}
+
 	} catch(err) {
 		console.error(err);
 		process.exit(1);
 	}
-	const url = `${process.env.API_ENDPOINT}?fields=provider_uid&filter[provider]=${process.env.PROVIDER}`;
-	console.log("Fetching articles", url);
-	var storedArticleData = (await axios.get( url, {
-		auth: {
-			username: process.env.API_USERNAME,
-			password: process.env.API_PASSWORD
-		}
-	})).data;
-	console.log("Fetched articles", storedArticleData.count);
-	var storedArticles = storedArticleData.data;
-
-	// Set up Axios
-	axios.defaults.headers.common = {'Authorization': `Bearer ${accessToken.token.access_token}`}
-	// Load the site
-	console.log("Loading Articles", process.env.URL);
-	try {
-		var articles = (await axios.get(process.env.URL)).data.assets;
-	} catch(err) {
-		console.error("Could not find articles");
-		throw(err);
-	}
-	
 	for (let article of articles) {
 		let exists = storedArticles.find(
 			storedArticle =>
 				storedArticle.provider_uid === article.id
 		);
-		if (exists) continue;
-		let fullArticle = (await axios.get(`${process.env.ARTICLE_URL}${article.id}`)).data.asset;
-		console.log(`Saving ${article.headline}`);
+		let fullArticle = (await axios.get(`${process.env.ARTICLE_URL}${article.id}`,
+			{
+				headers: Object.assign(headers, {
+					"Referer": "https://mercury.bloomberg.com/search",
+					"Cookie": `login=${accessToken}`
+				})
+			})).data.story;
+		// console.log(`Saving ${fullArticle}`);
 		let data = {
-			headline: fullArticle.fields.headline.value,
-			blurb: fullArticle.fields.description.value,
+			headline: fullArticle.headline.seo,
+			blurb: fullArticle.summary.seo,
 			provider_uid: article.id,
-			body: fullArticle.fields.text.value,
-			byline: fullArticle.fields.author.value,
-			provider: "Bloomberg",
-			date: fullArticle.published,
-			keywords: article.terminal_slug,
+			body: fullArticle.body.html,
+			byline: fullArticle.byline,
+			provider: process.env.PROVIDER,
+			date: fullArticle.publishedAt,
+			keywords: fullArticle.brand,
 		}
 		try {
-			await axios.post( process.env.API_ENDPOINT, data, {
-				auth: {
-					username: process.env.API_USERNAME,
-					password: process.env.API_PASSWORD
-				}
-			});
+			if (exists) {
+				console.log(`Updating existing article ${data.headline}`);
+				await axios.put(`${process.env.API_ENDPOINT}/article/${exists._id}`, data, {
+					auth: {
+						username: process.env.API_USERNAME,
+						password: process.env.API_PASSWORD
+					}
+				});
+			} else {
+				console.log(`Posting new article ${data.headline}`);
+				await axios.post( `${process.env.API_ENDPOINT}/article`, data, {
+					auth: {
+						username: process.env.API_USERNAME,
+						password: process.env.API_PASSWORD
+					}
+				});
+			}
 		} catch(err) {
 			console.error("Failed to save article", article.headline, article.uuid);
 			console.error(err);
 		}
 	}
-	console.log("Done")
+	console.timeEnd("Bloomberg");
 })();
